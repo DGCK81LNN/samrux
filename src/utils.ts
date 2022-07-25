770cd65e7f2011af6566187266946f8bb0e722de
DGCK81LNN <triluolnn@163.com>
2022-07-25 21:52:15 +0800

    add shuffle() to utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,16 @@
 export function escapeRegExp(source: string) {
   return source.replace(/[|\\{}()[\]^$+*?.]/g, "\\$&").replace(/-/g, "\\x2d")
 }
+
+/**
+ * Shuffles an array.
+ *
+ * This modiifes the array instead of creating a new one.
+ */
+export function shuffle<T>(array: T[]) {
+  for (let i = array.length - 1; i > 0; --i) {
+    const j = 0 | (Math.random() * (i + 1))
+    if (j !== i) [array[i], array[j]] = [array[j], array[i]]
+  }
+  return array
+}
--- test/utils.spec.ts
+++ test/utils.spec.ts
@@ -1,5 +1,5 @@
 import { strict as assert } from "node:assert"
-import { escapeRegExp } from "../src/utils"
+import { escapeRegExp, shuffle } from "../src/utils"
 
 describe("utils", function () {
   describe("escapeRegExp()", function () {
@@ -8,6 +8,29 @@ describe("utils", function () {
       const escaped = escapeRegExp(str)
       const re = new RegExp(`^${escaped}$`)
       assert.match(str, re)
+    })
+  })
+
+  describe("shuffle()", function () {
+    it("shuffles an array truly randomly", function () {
+      const arr = [1, 2, 3, 4, 5]
+      const expectedAverage = 3
+      const len = arr.length
+      const times = 2000
+      const maxError = 1000 / times
+
+      const sums = new Array(len).fill(0)
+      for (let i = times; i > 0; --i) {
+        const shuffled = shuffle(arr.slice(0))
+        for (let j = 0; j < len; ++j) sums[j] += shuffled[j]
+      }
+
+      const errors = sums.map(sum => sum / times - expectedAverage)
+
+      assert(
+        errors.every(error => Math.abs(error) < maxError),
+        `Expected average values of shuffled arrays to be ${expectedAverage} ± ${maxError}, got ${errors}`
+      )
     })
   })
 })
